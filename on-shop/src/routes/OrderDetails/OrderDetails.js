0a7aa34b82a46a903143e46b05952201629b593f
Gmike47 <galezra20@gmail.com>
2024-03-07 13:48:48 +0200

    add rest routes

---
+++ on-shop/src/routes/OrderDetails/OrderDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './OrderDetails.css';
+
+const OrderDetails = () => {
+    return (
+        <div className='order-details-container'>
+            <p className='order-details'>OrderDetails</p>
+        </div>
+    );
+}
+
+export default OrderDetails;
